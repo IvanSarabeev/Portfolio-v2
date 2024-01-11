b4d9e19671d9c82f8c8bc24aab1bbb70c74ee750
IvanSarabeev <ivan.sarabeev99@gmail.com>
2024-01-12 00:08:12 +0200

    chore: vercel/analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import { inject } from '@vercel/analytics';
+ 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+inject();
+
+inject({
+  beforeSend: (event) => {
+    if (event.url.includes('/private')) {
+      return null;
+    }
+    return event;
+  },
+});
