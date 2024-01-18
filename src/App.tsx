ee7f5c6670c7de34f5e4bdf13c0b1d9f5e7d0804
IvanSarabeev <ivan.sarabeev99@gmail.com>
2024-01-18 10:06:28 +0200

    build: contact section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from './pages/About'
 import Experience from './pages/Experience'
 import Projects from './pages/Projects'
 import ExtendProjects from './pages/ExtendProjects'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Experience />
         <Projects />
         <ExtendProjects />
+        <Contact />
       </Layout>
     </>
   )
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,27 @@
+import Button from "components/HTML/Button"
+
+const Contact = () => {
+  return (
+    <section id="contact" className=" max-w-2xl text-center mx-auto mb-28">
+        <h2 className="block font-monts regular-16 text-green mb-5">
+            <span className="num-headings">04.</span>
+            What's next ?
+        </h2>
+        <h3 className="contact-heading text-lightest-slate font-semibold mb-2.5">
+            Get In Touch
+        </h3>
+        <p className="text-pretty mb-12">
+            Although I’m not currently looking for any new opportunities, 
+            my inbox is always open. Whether you have a question or just want to say hi,
+            I’ll try my best to get back to you!
+        </p>
+        <a href="mailto:ivan.sarabeev99@gmail.com" target="_blank" className="size-full">
+            <Button className="contact-btn">
+                Say Hello
+            </Button>
+        </a>
+    </section>
+  )
+}
+
+export default Contact
