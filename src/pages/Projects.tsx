5799eab96769a9e9733e6a138df6dc2f3f4631a9
IvanSarabeev <ivan.sarabeev99@gmail.com>
2024-01-18 13:15:09 +0200

    style: design adjustments

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -13,16 +13,16 @@ const Projects = () => {
                 return (
                     <aside
                      key={item.id}
-                     className="relative gap-2.5 flex flex-col md:flex-row items-center odd:justify-start even:justify-end mb-12 md:mb-24 text-right odd:flex-row-reverse odd:text-left">
+                     className="relative gap-2.5 flex flex-col md:flex-row items-center odd:justify-start even:justify-end mb-12 md:mb-24 text-right odd:flex-col md:odd:flex-row-reverse odd:text-left">
                         <div className="project-img">
                             <a href={item.preview} rel="noopener noreferrer" target="_blank"
-                            className="size-full rounded align-middle bg-green before:absolute before:content-[''] before:size-full before:z-10 before:bg-navy before:mix-blend-screen">
+                            className="size-full rounded">
                                 <img 
                                     src={item.imgSrc}
                                     alt={item.title} 
                                     decoding="async"
                                     loading="lazy"
-                                    className="size-full rounded aspect-auto object-cover"
+                                    className="size-full rounded aspect-auto object-cover scale-100 md:scale-125 transition-all hover:scale-110 hover:shadow-2xl hover:rounded-md"
                                 />
                             </a>
                         </div>
@@ -39,7 +39,7 @@ const Projects = () => {
                             <ul className="relative z-10 flex flex-wrap items-center p-0 mt-6 mb-2.5 list-none">
                                     {item.stack.map((list) => {
                                         return(
-                                            <li key={list} className="regular-14 font-monts text-lightest-slate whitespace-nowrap ml-5 mb-1.5">
+                                            <li key={list} className="regular-14 font-monts text-lightest-slate whitespace-nowrap ml-5 mb-1.5 hover:text-green hover:underline transition-all cursor-default">
                                                 {list}
                                             </li>
                                         )
@@ -47,17 +47,19 @@ const Projects = () => {
                             </ul>
                             <div className="relative flex items-center justify-end text-lightest-slate mt-2.5">
                                 <a
-                                target="_blank"
-                                href={item.preview} 
-                                aria-label="GitHub-link"
-                                className="flex items-center justify-center p-2.5 transition-all hover:text-green">
+                                    target="_blank"
+                                    href={item.source} 
+                                    aria-label="GitHub-link"
+                                    className="flex items-center justify-center p-2.5 transition-all hover:text-green"
+                                    >
                                     <LuGithub />
                                 </a>
                                 <a
-                                target="_blank"
-                                href={item.source} 
-                                aria-label="External-link"
-                                className="flex items-center justify-center p-2.5 transition-all hover:text-green">
+                                    target="_blank"
+                                    href={item.preview} 
+                                    aria-label="External-link"
+                                    className="flex items-center justify-center p-2.5 transition-all hover:text-green"
+                                    >
                                     <CiShare1 />
                                 </a>
                             </div>
